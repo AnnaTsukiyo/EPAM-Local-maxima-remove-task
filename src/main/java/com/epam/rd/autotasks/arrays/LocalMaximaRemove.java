5672dcfee8f9c165166ccef7892f4a98954d5771
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-25 11:15:33 +0200

    done

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -10,10 +10,24 @@ public class LocalMaximaRemove {
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
-    public static int[] removeLocalMaxima(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
+    public static int[] removeLocalMaxima(int[] array) {
+        int result[] = new int[array.length];
+        int lastInd = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0 || i == array.length - 1) {
+                if (i == 0 && array[i] <= array[i + 1]) {
+                    result[lastInd++] = array[i];
+                } else if (i == array.length - 1 && array[i] <= array[i - 1]) {
+                    result[lastInd++] = array[i];
+                }
+                continue;
+            }
+            if (!(array[i] > array[i - 1] && array[i] > array[i + 1])) {
+                result[lastInd++] = array[i];
+            }
+        }
+        return Arrays.copyOfRange(result, 0, lastInd);
     }
 }
+
+
